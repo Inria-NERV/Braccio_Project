780c3ece36815d6011960b727837b81b1584643f
TristanVenot <48324026+TristanVenot@users.noreply.github.com>
2023-11-08 09:46:29 +0100

    Add files via upload

--- Tools/Burg_Connectivity.py
+++ Tools/Burg_Connectivity.py
@@ -14,9 +14,9 @@ from spectrum import arburg,arma2psd,pburg
 import statsmodels.regression.linear_model as transform
 from scipy.signal import spectrogram
 from visbrain.utils import morlet, normalization, averaging
-
+import time
 from numpy.fft import fft
-
+import multiprocessing
 from spectrum.correlation import CORRELATION
 from spectrum.covar import arcovar, arcovar_marple
 import spectrum.yulewalker as yulewalker
@@ -24,27 +24,36 @@ from spectrum.psd import ParametricSpectrum
 
 from multiprocessing import Pool
 
-def compute_psd(Epoch_compute,i,j,tab,xStart,xEnd,filter_order,N_FFT):
+class MultithreadVariables:
+    noverlap =  45
+    N_FFT =  500
+    f_max = 512
+    n_per_seg =  125
+    filter_order =  19
+
+
+def compute_psd(Epoch_compute,j,tab,xStart,xEnd,filter_order,N_FFT):
     Block_spectrum = []
     for numBlock in tab:
-        windowData = Epoch_compute[i, j, xStart[numBlock]:xEnd[numBlock]]
+        windowData = Epoch_compute[j, xStart[numBlock]:xEnd[numBlock]]
         windowData = signal.detrend(windowData, type='constant')
 
         AR, sigma2 = transform.burg(windowData, filter_order)
         PSD = arma2psd(-AR, NFFT=N_FFT, sides='centerdc')
-
+        # Record the end time
+        
         Block_spectrum.append(PSD)
-    return np.mean(Block_spectrum, axis=0)
+    return np.mean(Block_spectrum, axis=0),Block_spectrum
 
 
-def compute_psd_combined(Epoch_compute,i,j,eke,tab,xStart,xEnd,filter_order,N_FFT):
+def compute_psd_combined(Epoch_compute,j,eke,tab,xStart,xEnd,filter_order,N_FFT):
     Block_spectrum_yy = []
     Block_spectrum_xy = []
     for numBlock in tab:
-        windowData = Epoch_compute[i, j, xStart[numBlock]:xEnd[numBlock]]
+        windowData = Epoch_compute[ j, xStart[numBlock]:xEnd[numBlock]]
         windowData_xx = signal.detrend(windowData, type='constant')
 
-        windowData = Epoch_compute[i, eke, xStart[numBlock]:xEnd[numBlock]]
+        windowData = Epoch_compute[ eke, xStart[numBlock]:xEnd[numBlock]]
         windowData_yy = signal.detrend(windowData, type='constant')
 
         CS = np.correlate(windowData_xx, windowData_yy, "same")
@@ -55,18 +64,49 @@ def compute_psd_combined(Epoch_compute,i,j,eke,tab,xStart,xEnd,filter_order,N_FF
         AR, sigma2 = transform.burg(windowData_yy, filter_order)
         PSD = arma2psd(-AR, NFFT=N_FFT, sides='centerdc')
         Block_spectrum_yy.append(PSD)
-    return np.mean(Block_spectrum_xy, axis=0),np.mean(Block_spectrum_yy, axis=0)
+    return np.mean(Block_spectrum_xy, axis=0),np.mean(Block_spectrum_yy, axis=0),Block_spectrum_yy,Block_spectrum_xy
 
 
 def spectral_coh_burg_calculation(Epoch_compute,noverlap,N_FFT,f_max, n_per_seg,freqs_left,filter_order):
     #burg = pburg(Epoch_compute,15,NFFT = nfft_test)
+    print(N_FFT)
+    print(f_max)
+    print(n_per_seg)
+    print(filter_order)
+    print(noverlap)
+    MultithreadVariables.noverlap = noverlap
+    MultithreadVariables.N_FFT = N_FFT
+    MultithreadVariables.f_max = f_max
+    MultithreadVariables.n_per_seg = n_per_seg
+    MultithreadVariables.filter_order = filter_order
+    splitBranches = [Epoch_compute[i, :, :] for i in range(Epoch_compute.shape[0])]
+    print(MultithreadVariables.noverlap)
+    print(noverlap)
+    num_processes = multiprocessing.cpu_count()  # Use the number of CPU cores
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        results = list(pool.map(spectral_speed_up, splitBranches))
 
+    Full_Mat, Full_time = zip(*results)
+    return np.array(Full_Mat),np.array(Full_time)
+
+
+
+
+def spectral_speed_up(Epoch_compute):
+    #burg = pburg(Epoch_compute,15,NFFT = nfft_test)
+    noverlap = MultithreadVariables.noverlap
+    N_FFT = MultithreadVariables.N_FFT
+    f_max = MultithreadVariables.f_max
+    n_per_seg = MultithreadVariables.n_per_seg
+    filter_order = MultithreadVariables.filter_order
     a = Epoch_compute.shape
     print(a)
-    M = a[2]  # M = trial length, in samples
+    M = a[1]  # M = trial length, in samples
     L = n_per_seg  # L = windowing size, in samples
     noverlap = noverlap  # size of overlapping segment btw windows, in samples
+    print(noverlap)
     k = round((M-noverlap)/(L-noverlap))  # nb of windows in the trial
+
 
     # Arrays of starting and ending indices, for cutting the signal in overlapping chunks
     xStart = np.array(range(0, k*(L-noverlap), L-noverlap))
@@ -75,19 +115,22 @@ def spectral_coh_burg_calculation(Epoch_compute,noverlap,N_FFT,f_max, n_per_seg,
     fres = f_max/N_FFT
 
     tab = np.array(range(k))  # tab = indices of overlapping windows
-    trialspectrum = np.zeros([a[0],a[1],a[1],N_FFT])
-    with Pool(processes=4) as pool:
-        for i in range(a[0]):
-            for j in range(a[1]):
-                block_xx = pool.apply(compute_psd,(Epoch_compute,i,j,tab,xStart,xEnd,filter_order,N_FFT))
-                for eke in range(j+1,a[1]):
-                    block_xy,block_yy = pool.apply(compute_psd_combined,(Epoch_compute,i,j,eke,tab,xStart,xEnd,filter_order,N_FFT))
+    trialspectrum = np.zeros([a[0],a[0],N_FFT])
+    Time_FC = np.zeros([a[0],a[0],len(tab),N_FFT])
 
-                    trialspectrum[i, j,eke] = abs(block_xy)/np.sqrt(block_xx*block_yy)
+    for j in range(a[0]):
+        block_xx_ave,block_xx = compute_psd(Epoch_compute,j,tab,xStart,xEnd,filter_order,N_FFT)
+        for eke in range(j+1,a[0]):
+            block_xy_ave,block_yy_ave,block_yy,block_xy = compute_psd_combined(Epoch_compute,j,eke,tab,xStart,xEnd,filter_order,N_FFT)
 
-                #print(np.angle(np.array(Block_complex).mean(0)))
-    Full_Mat = trialspectrum[:,:,:,round(f_max/(2*fres)):round(f_max/fres)] + np.transpose(trialspectrum[:,:,:,round(f_max/(2*fres)):round(f_max/fres)],(0,2,1,3))
-    return Full_Mat
+            trialspectrum[j,eke] = abs(block_xy_ave)/np.sqrt(block_xx_ave*block_yy_ave)
+            block_tot = abs(np.array(block_xy))/np.sqrt(np.array(block_xx)*np.array(block_yy))
+            Time_FC[j,eke] = block_tot
+            #print(np.angle(np.array(Block_complex).mean(0)))
+    Full_Mat = trialspectrum[:,:,round(f_max/(2*fres)):round(f_max/fres)] + np.transpose(trialspectrum[:,:,round(f_max/(2*fres)):round(f_max/fres)],(1,0,2))
+    Full_time = Time_FC[:,:,:,round(f_max/(2*fres)):round(f_max/fres)] + np.transpose(Time_FC[:,:,:,round(f_max/(2*fres)):round(f_max/fres)],(1,0,2,3))
+    return Full_Mat,Full_time
+
 
 
 
